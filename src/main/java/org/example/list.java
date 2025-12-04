6f9039532e0407e57f67ee8c231ac6d52a901da3
doun9711-ai <doun9711@gmail.com>
2025-12-04 15:25:18 +0900

    list 저장

---
+++ src/main/java/org/example/list.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class list {
+    public static void main(String[] args) {
+        list motivation = new list();
+        
+    }
+    class motivation{
+        String saying;
+        String source;
+        int 번호;
+
+
+    }
+}
